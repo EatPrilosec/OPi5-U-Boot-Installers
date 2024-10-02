9e0aad98bc84670383ec504c3cb29784c57421c4
EatPrilosec <ae3nerdgod@gmail.com>
2024-10-02 14:35:03 -0400

    Update build.sh

--- spi-inst/build.sh
+++ spi-inst/build.sh
@@ -107,7 +107,7 @@ echo $BL31
 cd u-boot-cloner
 make mrproper
 make ${boardconfig}
-grep "BROM_BOOTSOURCE_SPINOR_RK3588 = 6" arch/arm/include/asm/arch-rockchip/bootrom.h && patch -p1 < v2-1-4-rockchip-rk3588-Fix-boot-from-SPI-flash.diff 
+#grep "BROM_BOOTSOURCE_SPINOR_RK3588 = 6" arch/arm/include/asm/arch-rockchip/bootrom.h && patch -p1 < v2-1-4-rockchip-rk3588-Fix-boot-from-SPI-flash.diff 
 make KCFLAGS="-fno-peephole2" CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc) || exit 1
 
 cp u-boot-rockchip-spi.bin $rootdir/out/u-boot-spi-cloner-$ubootRef-${boardName}__$orderUnder.bin
