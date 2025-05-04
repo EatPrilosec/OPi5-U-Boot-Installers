c9c791e4b83faa9b04726b76537637132782b6a0
EatPrilosec <ae3nerdgod@gmail.com>
2025-05-04 19:35:51 -0400

    Update build.sh

--- mainline/build.sh
+++ mainline/build.sh
@@ -14,7 +14,7 @@ if [[ "$ubootRepo" == *"custom_"* ]]; then
 fi
 
 sudo apt-get update
-sudo apt-get install gcc-12 gcc-12-aarch64-linux-gnu python3-pyelftools confget
+sudo apt-get install gcc-12 gcc-12-aarch64-linux-gnu python3-pyelftools confget libgnutls28-dev
 
 sudo ln -sf cpp-12 /usr/bin/cpp
 sudo ln -sf gcc-12 /usr/bin/gcc
